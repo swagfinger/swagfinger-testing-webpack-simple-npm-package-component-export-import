b7b7ccac67f09f14f407cf6b171c0e4e9021f37c
swagfinger <swagfinger@gmail.com>
2023-01-24 17:23:13 +0800

    component webpack / package setup for consumption by component-import

--- component/src/App.js
+++ component/src/App.js
@@ -4,13 +4,10 @@ import { Input } from './components/Input';
 const App = () => {
   const [value, setValue] = useState('');
   const onChangeHandler = (e) => {
+    console.log(e.target.value);
     setValue(e.target.value);
   };
-  return (
-    <>
-      <Input value={value} onChange={onChangeHandler} />
-    </>
-  );
+  return <Input value={value} onChange={onChangeHandler} />;
 };
 
 export default App;
--- component/webpack.config.js
+++ component/webpack.config.js
@@ -1,29 +1,41 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  return {
+    mode: isProduction ? 'production' : 'development',
+    devServer: {
+      static: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000,
+    },
+    entry: isProduction
+      ? './src/index.production.js'
+      : './src/index.development.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js',
+      library: 'Input',
+      libraryTarget: 'umd',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        filename: './index.html',
+        inject: true,
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: './index.html',
-      inject: true,
-    }),
-  ],
+  };
 };
