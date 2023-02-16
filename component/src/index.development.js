62fdf3245e5b1b23f46f698d4818d6f065b7963e
swagfinger <swagfinger@gmail.com>
2023-02-16 13:45:42 +0800

    polish dev

--- component/src/index.development.js
+++ component/src/index.development.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+
+import { Input } from './components/Input';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab='home' />);
+
+const InputExample = () => {
+  const [value, setValue] = useState('');
+  const onChangeHandler = (e) => {
+    console.log(e.target.value);
+    setValue(e.target.value);
+  };
+
+  return <Input value={value} onChange={onChangeHandler} />;
+};
+
+root.render(<InputExample />);
